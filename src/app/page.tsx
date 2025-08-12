ce970c21ba72fb427966ffc1e79ab45a07d669c3
aceky <tafinasoa35@gmail.com>
2025-08-13 00:34:15 +0300

    Refactor `Navbar` for cleaner structure and styling updates; enhance section navigation with IDs; improve `ItemSwiper`, `Features`, and `Testimonials` alignment and responsiveness.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import {FeaturesHighlight} from "@/components/sections/FeaturesHighlight";
 import Features from "@/components/sections/Features";
 import Testimonials from "@/components/sections/Testimonials";
 import {CTA} from "@/components/sections/CTA";
-import {CircleBg} from "@/components/CircleBg";
 
 export default function Page() {
   return (
@@ -13,9 +12,6 @@ export default function Page() {
         <FeaturesHighlight/>
         <Testimonials/>
         <CTA/>
-        {/*<CircleBg/>*/}
-        {/*<HowItWorks/>*/}
-        {/*<Events/>*/}
       </>
   )
 }
