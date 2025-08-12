8ef3013f80af3932595d95700aef78a0a0eb57c7
JeanCharlesMEILLAND <jc.meilland@idboats.com>
2025-08-12 21:48:01 +0200

    fixe

---
+++ .history/src/app/page_20250812214557.tsx
@@ -0,0 +1,19 @@
+import Hero from '@/components/sections/Hero'
+import Features from '@/components/sections/Features'
+import HowItWorks from '@/components/sections/HowItWorks'
+import Testimonials from '@/components/sections/Testimonials'
+import Events from '@/components/sections/Events'
+import {FeaturesHighlight} from "@/components/sections/FeaturesHighlight";
+
+export default function Page() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <FeaturesHighlight/>
+      <HowItWorks />
+      <Events />
+      <Testimonials />
+    </>
+  )
+}
